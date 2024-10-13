dec7c9187227e47472988a13be5f09dc0c92a12b
David Kahurani <redress@duck.com>
2024-10-13 10:36:35 +0300

    Format this
    It actually contains loads and load of whitespace but that's for another day
    
    Signed-off-by: David Kahurani <redress@duck.com>

--- brokensweeper.php
+++ brokensweeper.php
@@ -116,72 +116,66 @@ class ListManager {
 public function __constructor() {}
 
 public function generate() {
-
-static $bucket = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'];
-static $upperbucket = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z'];
-static $vowels = ['a', 'e', 'i', 'o', 'u'];
-
-/* 20 items */
-$items = 20;
-
-while ($items != 0) {
-
-/* Generate the "name" of artist first and foremost */
-$lettercountfirst = 0;
-$lettercountsecond = 0;
-$firstname = "";
-$lastname = "";
-
-while($lettercountfirst <= 4 ) { $lettercountfirst = random_int(0, 15);}
-while($lettercountsecond <= 4) { $lettercountsecond = random_int(0, 15);} 
-
-
-/* There is five vowels and 20 consonants so one way or the other we have to force the vowels. */
-while($lettercountfirst > 0) {
-
-$firstname[$lettercountfirst] = $bucket[random_int(0, 15)];
-
-/* fallback to force vowels in case one is needed */
-for($start = 0; $start <= 4; $start++) {
-
-if ($firstname[$lettercountfirst] == $vowels[$start]) break;
-
-else if ($start == 4) { $lettercountfirst = $lettercountfirst - 1; $firstname[$lettercountfirst] = $vowels[rand() % 4]; }
-}
-
-$lettercountfirst--;
-
-}
-while($lettercountsecond > 0) {
-
-$lastname[$lettercountsecond] = $bucket[random_int(0, 15)];
-
-for ($start = 0; $start <= 4; $start++) {
-
-if ($lastname[$lettercountsecond] == $vowels[$start]) break;
-
-else if ($start == 4) { $lettercountsecond = $lettercountsecond - 1; $lastname[$lettercountsecond] = $vowels[rand() % 4]; }
-
-}
-
-$lettercountsecond--;
-
-}
-
-/* it doesn't seem like PHP has methods for handling characters unless you consider those meant for dealing with UNICODE */
-$firstname[0] = $upperbucket[random_int(0, 25)];
-$lastname[0] = $upperbucket[random_int(0, 25)];
-
-echo $firstname . " " . $lastname . "<br>"; 
-
-/* Generate the name of the track */
-$words = random_int(0, 5);  if ($words == 0) $words = 1;
-
-while($words != 0) { break;}
-
-$items = $items - 1;
-}
-
+	static $bucket = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'];
+	static $upperbucket = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z'];
+	
+	static $vowels = ['a', 'e', 'i', 'o', 'u'];
+	
+	/* 20 items */
+	$items = 20;
+	
+	while ($items != 0) {
+		/* Generate the "name" of artist first and foremost */
+		$lettercountfirst = 0;
+		$lettercountsecond = 0;
+		$firstname = "";
+		$lastname = "";
+		
+		while($lettercountfirst <= 4 ) { $lettercountfirst = random_int(0, 15);}
+		while($lettercountsecond <= 4) { $lettercountsecond = random_int(0, 15);} 
+		
+		/* There is five vowels and 20 consonants so one way or the other we have to force the vowels. */
+		while($lettercountfirst > 0) {
+			$firstname[$lettercountfirst] = $bucket[random_int(0, 15)];
+			/* fallback to force vowels in case one is needed */
+			for($start = 0; $start <= 4; $start++) {
+				if ($firstname[$lettercountfirst] == $vowels[$start])
+				       	break;
+				else if ($start == 4) { 
+					$lettercountfirst = $lettercountfirst - 1; 
+					$firstname[$lettercountfirst] = $vowels[rand() % 4];
+			       	}
+			}
+			
+			$lettercountfirst--;
+		}
+		
+		while($lettercountsecond > 0) {
+			$lastname[$lettercountsecond] = $bucket[random_int(0, 15)];
+			
+			for ($start = 0; $start <= 4; $start++) {
+				
+				if ($lastname[$lettercountsecond] == $vowels[$start]) 
+					break;
+				else if ($start == 4) { 
+					$lettercountsecond = $lettercountsecond - 1; $lastname[$lettercountsecond] = $vowels[rand() % 4]; 
+				}
+			}
+			$lettercountsecond--;
+		}
+		
+		/* it doesn't seem like PHP has methods for handling characters unless you consider those meant for dealing with UNICODE */
+		$firstname[0] = $upperbucket[random_int(0, 25)];
+		$lastname[0] = $upperbucket[random_int(0, 25)];
+		
+		echo $firstname . " " . $lastname . "<br>"; 
+		
+		/* Generate the name of the track */
+		$words = random_int(0, 5);  if ($words == 0) $words = 1;
+		
+		while($words != 0) { break;}
+		$items = $items - 1;
+	}
 }
 
 private $playlist_generated;
