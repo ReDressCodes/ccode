175dea697624a20382e4304af02b8902fe8260c7
David Kahurani <redress@duck.com>
2024-11-13 12:03:54 +0300

    This is a lame attempt at detecting which words on the English language are keywords in PHP
    Signed-off-by: David Kahurani <redress@duck.com>

---
+++ PHP/keywords.php
@@ -0,0 +1,27 @@
+<?php 
+
+  phpinfo();
+
+  $correct = 'aaxxbbccddwwddffgg';
+
+/* foreach($correct as $character) { echo $character; } */
+
+  switch($correct)
+  {
+  case 'a':
+	  echo "hello\n";
+	  break;
+  case 'b':
+	  echo "black\n";
+	  break;
+  case 'c':
+	  echo "yellow\n";
+	  break;
+  case 'aaxxbbccddwwddffgg':
+	  echo "whole string\n";
+	  break;
+  default:
+	  echo "none";
+  }
+
+?>
