b6acc564c9c8b16c2b9b035538353a92e409f3b1
David Kahurani <redress@duck.com>
2025-03-03 11:53:16 +0300

    common sorting algorithms
    Signed-off-by: David Kahurani <redress@duck.com>

--- c/sort.c
+++ c/sort.c
@@ -18,6 +18,18 @@ bool is_space(char c)
 	return c == ' ' || c == '\t' || c == '\v' || c == '\r';
 }
 
+typedef int (*compare)(void *a, void *b);
+
+typedef void (*selection_sort)(void *ptr, int elements, size_t element_size, compare fn);
+
+typedef void (*quick_sort)(void *ptr, int elements, size_t element_size, compare fn);
+
+typedef void (*bubble_sort)(void *ptr, int elements, size_t element_size, compare fn);
+
+typedef void (*heap_sort)(void *ptr, int elements, size_t element_size, compare fn);
+
+typedef void (*merge_sort)(void *ptr, int elements, size_t element_size, compare fn);
+
 int main(int argc, char *argv[])
 {
 	char *str = "The quick brown fox jumps over the lazy dog";
