37e5cd755b0bc88920c4b2fc43d1382bd28a773c
David Kahurani <redress@duck.com>
2025-03-03 11:27:05 +0300

    unique_ptr(smart pointer(s)) operator overloading for '->'
    Signed-off-by: David Kahurani <redress@duck.com>

---
+++ cpp/unique.cc
@@ -0,0 +1,35 @@
+#include <cstdio>
+#include <iostream>
+#include <memory>
+ 
+// helper class for runtime polymorphism demo below
+struct B
+{
+    virtual ~B() = default;
+ 
+    virtual void bar() { std::cout << "B::bar\n"; }
+};
+ 
+struct D : B
+{
+    D() { std::cout << "D::D\n"; }
+    ~D() { std::cout << "D::~D\n"; }
+ 
+    void bar() override { std::cout << "D::bar\n"; }
+
+	private:
+    int newbie;
+};
+ 
+// a function consuming a unique_ptr can take it by value or by rvalue reference
+std::unique_ptr<D> pass_through(std::unique_ptr<D> p)
+{
+    p->bar();
+    p.newbie = 20;
+    return p;
+}
+
+int main(int argc, char *argv[])
+{
+	return 0;
+}
