65d8f718b621375860f9d50261d03fa8d923f82b
David Kahurani <redress@duck.com>
2025-03-24 15:41:31 +0300

    multi-threaded file reader, not tested YET
    Signed-off-by: David Kahurani <redress@duck.com>

---
+++ cpp/filereader.cpp
@@ -0,0 +1,80 @@
+#include <iostream>
+#include <fstream>
+#include <mutex>
+#include <thread>
+#include <vector>
+
+class Reader {
+
+	public:
+	Reader(size_t read_size)
+	{
+		this->read_size = read_size;
+
+		this->file = new std::fstream("irclog.txt", std::fstream::in | std::fstream::out);
+		this->file_size = file->tellg();
+
+		for (int i = 0; i < 8; i++){
+			threads.push_back(new std::thread(Reader::Read, std::ref(this->read_size)));
+		}
+	}
+
+	~Reader()
+	{
+		for (auto &t: this->threads) { t->join(); }
+
+		for (auto t: this->threads) { delete t; }
+
+		this->file->close();
+		delete file;
+
+		this->Output();
+	}
+
+	static void Read(size_t read_size)
+	{
+		mutex.lock();
+
+		size_t to_read = file_size - already_read;
+
+		if (to_read > read_size) 
+		{ 
+			to_read = read_size; 
+		}
+
+		std::string data(to_read, '\0');
+		file->read(&data[0], to_read);
+
+		file_contents.push_back(data);
+
+		already_read += to_read;
+
+		mutex.unlock();
+	}
+
+	void Output()
+	{
+		for(auto &s: this->file_contents) { std::cout << s; }
+	}
+
+private:
+	inline static size_t read_size;
+	inline static size_t file_size;
+	inline static size_t already_read;
+
+	std::vector<std::thread*> threads;
+	/* protect the private members against concurrent access by multiple threads */
+	inline static std::mutex mutex;
+	inline static std::fstream *file;
+	inline static std::vector<std::string> file_contents;
+};
+
+int main(int argc, char *argv[])
+{
+
+	Reader *reader = new Reader(1024);
+
+	delete reader;
+
+	return 0;
+}
